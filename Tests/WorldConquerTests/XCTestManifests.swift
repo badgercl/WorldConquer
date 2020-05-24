afdb4fc85fc33b2e17a54e73afa6182a29dcfb63
Alfredo Cadiz <4398862+badgercl@users.noreply.github.com>
2020-05-24 03:47:05 +0200

    fixing linux tests

--- Tests/WorldConquerTests/XCTestManifests.swift
+++ Tests/WorldConquerTests/XCTestManifests.swift
@@ -4,7 +4,13 @@ import XCTest
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(JsonWorldProviderTests.allTests),
-
+        testCase(RandomClosenessTests.allTests),
+        testCase(RandomWinningTerritoryCalculatorTests.allTests),
+        testCase(EngineTests.allTests),
+        testCase(ContinentTests.allTests),
+        testCase(CountryTests.allTests),
+        testCase(TerritoryTests.allTests),
+        testCase(JsonWorldProviderTests.allTests)
     ]
 }
 #endif
