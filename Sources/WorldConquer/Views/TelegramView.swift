d1e8ee62099efbd586bb08958a8a6fdd7bf8ad29
Alfredo Cadiz <4398862+badgercl@users.noreply.github.com>
2020-05-28 19:29:00 +0200

    fixed Telegram start message

--- Sources/WorldConquer/Views/TelegramView.swift
+++ Sources/WorldConquer/Views/TelegramView.swift
@@ -27,10 +27,7 @@ public struct TelegramView: View {
     }
 
     private func showStartGame(_ world: World) {
-        let worldStatus: String = world.countries.map {
-            let territories = $0.territories.map(\.name).joined(separator: ", ")
-            return " - *\($0.name)* (\($0.territories.count)): [\(territories)]"
-        }.joined(separator: "\n")
+        let worldStatus: String = world.continents.map { "*\($0.name)*: [\($0.countries.map(\.name).joined(separator: ", "))]" }.joined(separator: "\n")        
         let text = "Game started with World:\n\(worldStatus)"
         sendMessage(text: text)
     }
