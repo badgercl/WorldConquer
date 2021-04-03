becb9cd31a67a674f1e02f04bfb6f74ef6a26015
Alfredo Cadiz <4398862+badgercl@users.noreply.github.com>
2021-04-03 22:35:11 +0200

    Removing unneeded comments

--- Sources/WorldConquer/Engine/ClosenessCalculators/ClosenessCalculator.swift
+++ Sources/WorldConquer/Engine/ClosenessCalculators/ClosenessCalculator.swift
@@ -1,10 +1,3 @@
-//
-//  File.swift
-//  
-//
-//  Created by Alfredo Cadiz on 20.05.20.
-//
-
 import Foundation
 
 public protocol ClosenessCalculator {
--- Sources/WorldConquer/Engine/WinningTerritoryCalculators/WinningCountryCalculator.swift
+++ Sources/WorldConquer/Engine/WinningTerritoryCalculators/WinningCountryCalculator.swift
@@ -1,10 +1,3 @@
-//
-//  File.swift
-//  
-//
-//  Created by Alfredo Cadiz on 21.05.20.
-//
-
 import Foundation
 
 public protocol WinningTerritoryCalculator {
