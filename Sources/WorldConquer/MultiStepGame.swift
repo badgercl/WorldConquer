9cd5dc9a3396d6354e00ad2899692cc5a3d91b26
Alfredo Cadiz <4398862+badgercl@users.noreply.github.com>
2021-04-07 19:00:51 +0200

    updating multistep game

--- Sources/WorldConquer/MultiStepGame.swift
+++ Sources/WorldConquer/MultiStepGame.swift
@@ -1,22 +1,31 @@
 import Foundation
+import Logging
 
 public final class MultiStepGame: Game {
     private let engine: Engine
     private let viewsManager: ViewsManager
     private let stepTime: UInt32
     
-    public init(worldProvider: WorldProvider,
+    public init(worldFilePath: String?,
                 closenessCalculator: ClosenessCalculator,
                 winningTerritoryCalculator: WinningTerritoryCalculator,
                 views: [View],
-                stepTime: Int) {
-        engine = Engine(world: worldProvider.generate(),
-                             closenessCalculator: closenessCalculator,
-                             winningTerritoryCalculator: winningTerritoryCalculator)
+                logger: Logger?,
+                stepTime: Int,
+                persistency: WorldPersistency = SingleFileWorldPersistency(jsonWorldProvider: JsonWorldProvider())) throws {
+        guard let world = persistency.load(from: worldFilePath) else {
+            throw GameError.invalidFile
+        }
+        engine = Engine(world: world,
+                        closenessCalculator: closenessCalculator,
+                        winningTerritoryCalculator: winningTerritoryCalculator)
         viewsManager = ViewsManager(views: views)
         self.stepTime = UInt32(stepTime)
+        if let logger = logger {
+            appLogger = logger
+        }
     }
-
+    
     public func start() {
         viewsManager.render(.start(engine.currentWorld))
         while true {
